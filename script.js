cf98ccc0cea4f12a485571a6ebac307a6419c64a
Simonot Antoine <simonotantoine1@gmail.com>
2022-10-21 16:25:52 +0200

    chubby chucky finished

--- script.js
+++ script.js
@@ -5,27 +5,35 @@ document.addEventListener('DOMContentLoaded', async function () {
     let wall = document.getElementById('disappearing-wall')
     let camera = document.getElementById('camera')
     let cameraPos = camera.getAttribute('position')
+    let body = document.querySelector('body')
+
+    function blink() {
+        body.style.transition = '0.5s'
+        body.style.background = 'black'
+        body.style.opacity = 0
+    }
 
     function removeWall() {
         return new Promise((resolve, reject) => {
-            let body = document.querySelector('body')
-
-            body.style.transition = '0.5s'
-            body.style.background = 'black'
-            body.style.opacity = 0
+            blink()
 
             setTimeout(() => {
+                console.log('test')
                 wall.parentNode.removeChild(wall)
                 body.style.background = 'white'
                 body.style.opacity = 1
                 resolve()
-            }, 2000)
+            }, 1000)
         })
     }
 
-    function rotateCamera() {
+    function rotateCamera(rotation) {
         return new Promise((resolve, reject) => {
-            camera.setAttribute('animation', 'property: rotation; to: 0 360 0; dur: 5000; easing: linear; loop: false;')
+            camera.setAttribute(
+                'animation',
+                `property: rotation; to: 0 ${rotation} 0; dur: 5000; easing: linear; loop: false;`
+            )
+
             setTimeout(() => {
                 resolve()
             }, 5000)
@@ -44,11 +52,84 @@ document.addEventListener('DOMContentLoaded', async function () {
         })
     }
 
-    moveCamera(-10).then(() => {
-        rotateCamera().then(() => {
-            removeWall().then(() => {
-                moveCamera(-30)
+    function chuckyEvent() {
+        return new Promise((resolve, reject) => {
+            let chuckyContainer = document.getElementById('chucky-container')
+            chuckyContainer.setAttribute('visible', 'true')
+
+            document.addEventListener('click', () => {
+                setTimeout(() => {
+                    resolve()
+                }, 5000)
             })
         })
-    })
+    }
+
+    function ghostEvent() {
+        return new Promise((resolve, reject) => {
+            let ghostSound = document.getElementById('ghost-sound')
+            let ghosts = document.querySelectorAll('.ghostItem')
+            document.addEventListener(
+                'click',
+                () => {
+                    ghostSound.play()
+                    ghosts.forEach((ghost) => {
+                        ghost.setAttribute(
+                            'animation',
+                            'property: position; to: 0 0 0; dur: 5000; easing: linear; loop: false;'
+                        )
+                    })
+                    setTimeout(() => {
+                        resolve()
+                    }, 5000)
+                },
+                { once: true }
+            )
+        })
+    }
+
+    function tpToNextRoom(z) {
+        return new Promise((resolve, reject) => {
+            document.addEventListener(
+                'click',
+                () => {
+                    blink()
+                    setTimeout(() => {
+                        body.style.background = 'white'
+                        body.style.opacity = 1
+                        camera.setAttribute('position', `0 4 ${z}`)
+                        resolve()
+                    }, 1000)
+                },
+                { once: true }
+            )
+        })
+    }
+
+    let started = false
+    if (started === false) {
+        document.addEventListener(
+            'click',
+            async function () {
+                started = true
+                moveCamera(-10).then(() => {
+                    rotateCamera(360).then(() => {
+                        removeWall().then(() => {
+                            moveCamera(-42.5).then(() => {
+                                tpToNextRoom(-60).then(() => {
+                                    moveCamera(-160).then(() => {
+                                        chuckyEvent()
+                                        blink()
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            },
+            {
+                once: true,
+            }
+        )
+    }
 })
